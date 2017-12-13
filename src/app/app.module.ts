aedc2bd5ea193c9da617026c89b655461d622974
Mathieu Boisnard <m.boisnard@free.fr>
2017-12-13 10:34:22 +0100

    Add routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import {LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StepsComponent} from './steps/steps.component';
+import {AppRoutingModule} from './app.routing';
+import {ProductsComponent} from './products/products.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    StepsComponent,
+    ProductsComponent
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: PathLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {StepsComponent} from './steps/steps.component';
+import {ProductsComponent} from './products/products.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'steps', pathMatch: 'full' },
+  { path: 'steps', component: StepsComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: '**', redirectTo: 'steps'}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
